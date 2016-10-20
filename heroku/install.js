0c8bf677102428a568a3117d725e59737c6645b9
j.laville <j.laville@qwant.com>
2016-10-20 10:02:32 +0200

    add

--- heroku/install.js
+++ heroku/install.js
@@ -2,4 +2,4 @@ var sqlite3 = require("sqlite3")
 var dbPath = path.join(__dirname, '..', "kvs.db")
 var dbkv = new sqlite3.Database(dbPath);
 
-sqlite3
+sqlite3.run("CREATE TABLE IF NOT EXIST key_value")
